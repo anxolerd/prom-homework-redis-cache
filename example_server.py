a030c467ffdb144afbb885289c03c35e7b93489a
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-29 14:32:44 +0300

    simple server

---
+++ example_server.py
@@ -0,0 +1,13 @@
+from wsgiref.simple_server import make_server
+from cgi import parse_qs
+import time
+
+
+def simple_app(environ, start_response):
+    time.sleep(2)
+    start_response('200 OK', [('Content-type', 'text/plain')])
+    parameters = parse_qs(environ.get('QUERY_STRING', ''))
+    return ["%s = %s\n" % (k, v) for (k, v) in parameters.iteritems()]
+
+if __name__ == '__main__':
+    make_server('', 8000, simple_app).serve_forever()
